c959bfc0745dcc79560772645829cc39e2d07a6d
BengiAgcal <bengiagcal@gmail.com>
2022-11-22 01:51:08 -0800

    more fish

---
+++ Assets/3dObj/Freshwater fish/Scripts/Shoal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoal : MonoBehaviour {
+
+	public float speed;
+	public ShoalManager manager;
+	public int rotSpeed = 1;
+	private int water;
+
+
+	// Use this for initialization
+	void Start () {
+		water = manager.waterLevel;
+		speed = Random.Range(manager.minSpeed,manager.maxSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print(Terrain.activeTerrain.SampleHeight(transform.position));
+
+		GameObject[] gos;
+		gos = manager.allFish;
+		Vector3 vavoid = Vector3.zero;
+		float nDistance;
+		
+		
+		
+
+		foreach (GameObject go in gos)
+		{
+			if(go!= this.gameObject)
+			{
+				nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+				
+					if(nDistance < 1.0f)
+					{
+						vavoid = vavoid + (this.transform.position - go.transform.position);
+					}
+			}
+		}
+
+		Vector3 direction = manager.goalActual + vavoid- this.transform.position;
+		if(transform.position.y < Terrain.activeTerrain.SampleHeight(transform.position) + ShoalManager.terrainOffset+2)
+		{
+			
+			direction.y = 1;
+		}
+		if (transform.position.y > water-2)
+		{
+			
+			direction.y = -1;
+		}
+		
+		if (Random.Range(0,1000)<10)
+			speed = Random.Range(manager.minSpeed,manager.maxSpeed);
+
+		transform.Translate(0,0,Time.deltaTime*speed);
+		transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(direction),rotSpeed * Time.deltaTime);
+	}
+
+	
+}
---
+++ Assets/3dObj/Freshwater fish/Scripts/ShoalManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoalManager : MonoBehaviour {
+
+	public Terrain terrain;
+	public GameObject fishPrefab;
+	public Vector3 goal1;
+	public Vector3 goal2;
+	public Vector3 goalActual;
+	public int waterLevel;
+	public Vector3 swimLimits = new Vector3(5,5,5);
+	private float timer = 5;
+	public int numFish = 1;
+	public GameObject[] allFish;
+	[Header("Fish Settings")]
+	[Range(0.0f,5.0f)]
+	public float minSpeed;
+	[Range(0.0f,5.0f)]
+	public float maxSpeed;
+	public static float terrainOffset;
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		terrainOffset = terrain.transform.position.y;
+		allFish = new GameObject[numFish];
+		for(int i =0; i < numFish; i++)
+		{
+			Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x,swimLimits.x),
+																Random.Range(-swimLimits.y,swimLimits.y),
+																Random.Range(-swimLimits.z,swimLimits.z));
+			if (pos.y < (Terrain.activeTerrain.SampleHeight(pos)+terrainOffset))
+			{
+				pos.y = Terrain.activeTerrain.SampleHeight(pos)+terrainOffset;
+			}
+			if (pos.y > waterLevel)
+			{
+				pos.y = waterLevel;
+			}
+
+
+			allFish[i] = (GameObject) Instantiate (fishPrefab,pos,Quaternion.identity);
+			allFish[i].GetComponent<Shoal>().manager = this;
+	}
+}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -=Time.deltaTime;
+		if(timer<= 0){
+			NewGoal();
+			
+			timer = (Random.Range(4,7));
+
+			if (Random.Range(0,3) == 0)
+			{
+				goalActual = goal1;
+			}
+			else
+			{
+				goalActual = goal2;
+			}
+		}
+	}
+
+	void NewGoal()
+	{
+		goal1 = this.transform.position + new Vector3(Random.Range(-swimLimits.x,swimLimits.x),Random.Range(terrainOffset,waterLevel),Random.Range(-swimLimits.z,swimLimits.z));															
+		goal2 = this.transform.position + new Vector3(Random.Range(-swimLimits.x,swimLimits.x),Random.Range(terrainOffset,waterLevel),Random.Range(-swimLimits.z,swimLimits.z));	
+	}
+}
